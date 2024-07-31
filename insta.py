c0f26b0878da6452eeae221daf1bef8d5cca9cb1
ragavadk1 <74495739+ragavadk1@users.noreply.github.com>
2024-07-31 22:38:07 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -35,7 +35,7 @@ headers = {
 
 session.headers = headers
 data = {
-    'broadcast_message': '┗━━━━━━⊱ 𝑨𝒔𝒔𝒂𝒔𝒔𝒊𝒏'𝒔 𝑪𝒓𝒆𝒆𝒅 𝑶𝒅𝒚𝒔𝒔𝒆𝒚 ⊰━━━━━━┛',
+    'broadcast_message': '𝑨𝒔𝒔𝒂𝒔𝒔𝒊𝒏'𝒔 𝑪𝒓𝒆𝒆𝒅 𝑶𝒅𝒚𝒔𝒔𝒆𝒚',
     'internal_only': 'false',
     'preview_height': '1920',
     'preview_width': '1080',
